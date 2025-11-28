5cdcca63f1dbc6c478ee7a92eed742b8393d45af
Alexey Kotov <rubi_ru@vk.com>
2025-11-28 18:19:50 +0300

    Добавлена закрывающая скобка

--- main.cpp
+++ main.cpp
@@ -2,3 +2,17 @@
 
 int main() {
     std::cout << "Hello, Git!" << std::endl;
+}
+
+/*
+
+    # Добавить файл main.cpp.
+    git add main.cpp
+
+    # Добавить все изменения репозитория.
+    git add --all
+
+    # Добавить все изменения директории.
+    git add . 
+
+*/
